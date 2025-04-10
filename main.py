a0fec223b0c311d281330d2072c0c183081b7774
fahadcoder356 <fahad.dcc24@gmail.com>
2025-04-10 10:28:42 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def check_monsoon():
+    st.title('Monsoon Checker')
+    st.write('এখানে মন্সুন চেক করার জন্য ফর্ম বা তথ্য থাকবে।')
+
+if __name__ == '__main__':
+    check_monsoon()
